0555bec996dce5cf9398ec070c8dcfeab0713b88
Zhanahmet01p <ganahmet05@mail.ru>
2024-11-13 18:49:19 +0600

    test

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles/index.scss';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div></div>
+    </React.Fragment>
+  );
+}
+
+export default App;
